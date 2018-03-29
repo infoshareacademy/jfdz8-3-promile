f34ef486956ce60d37fbc984e2c624df550ff27c
Michal Muszynski <m.muszy@gmail.com>
2018-03-29 19:10:44 +0200

    Navbar and Form sections refined

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,5 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
-    }, 1500);
+    }, 1400);
 });
