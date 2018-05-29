3c35cee9e6aeb323383b1c440dba600ba5ceffb5
Michal Muszynski <m.muszy@gmail.com>
2018-05-29 21:36:57 +0200

    Span creator function

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,10 +1,21 @@
 const heroImg = document.querySelector('.hero__image');
+const heroDots = document.querySelector('.hero__dots');
 const heroDot = document.getElementsByClassName('image_dot');
 let leftButton = document.querySelector('.left_arrow');
 let rightButton = document.querySelector('.right_arrow');
 const imgLocations = ['assets/images/slideshow_photo/gdansk.jpg',
                       'assets/images/slideshow_photo/gdynia.jpg',
-                      'assets/images/slideshow_photo/sopot2.png'];
+                      'assets/images/slideshow_photo/sopot2.png',];
+
+function createDots() {
+    for(var i =0; i < imgLocations.length; i++) {
+        const spanDot = document.createElement('span');
+        spanDot.classList = "image_dot";
+        heroDots.appendChild(spanDot)
+    }
+}
+
+createDots();
 let sliderNum = 0;
 
 function imgChange(x) {
@@ -17,7 +28,6 @@ function imgChange(x) {
             heroImg.src = imgLocations[sliderNum];
         }
     } else {
-        console.log(sliderNum);
         if (sliderNum === 0) {
             sliderNum = imgLocations.length - 1;
             heroImg.src = imgLocations[sliderNum];
@@ -26,6 +36,7 @@ function imgChange(x) {
             heroImg.src = imgLocations[sliderNum];
         }
     }
+    console.log(sliderNum);
 }
 
 rightButton.addEventListener('click', function() {
