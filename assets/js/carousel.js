fa586c26f5b42ee484ce983a08fe693d49b04570
Michal Muszynski <m.muszy@gmail.com>
2018-05-30 20:48:15 +0200

    New version of carousel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -6,52 +6,58 @@ let rightButton = document.querySelector('.right_arrow');
 const imgLocations = ['assets/images/slideshow_photo/gdansk.jpg',
                       'assets/images/slideshow_photo/gdynia.jpg',
                       'assets/images/slideshow_photo/sopot2.png',];
-
 function createDots() {
     for(var i =0; i < imgLocations.length; i++) {
         const spanDot = document.createElement('span');
         spanDot.classList = "image_dot";
         heroDots.appendChild(spanDot)
-
     }
     heroDot[0].classList.add("image_dot_active")
 }
-
 createDots();
-let sliderNum = 0;
 
-function imgChange(x) {
-    for (var i = 0; i < heroDot.length; i++) {
-        heroDot[i].classList.remove("image_dot_active")
-    }
-    if (x > 0) {
-        if (sliderNum === imgLocations.length - 1) {
-            sliderNum = 0;
-            heroImg.src = imgLocations[sliderNum];
-        } else {
-            sliderNum += x;
-            heroImg.src = imgLocations[sliderNum];
-        }
-    } else {
-        if (sliderNum === 0) {
-            sliderNum = imgLocations.length - 1;
-            heroImg.src = imgLocations[sliderNum];
-        } else {
-            sliderNum += x;
-            heroImg.src = imgLocations[sliderNum];
-        }
-    }
+function addClass() {
     for(var i = 0; i < heroDot.length; i++) {
         heroDot[sliderNum].classList.add("image_dot_active");
     }
+}
+
+function clearClass() {
+    for (var i = 0; i < heroDot.length; i++) {
+        heroDot[sliderNum].classList.remove("image_dot_active")
+    }
+}
+
+
+
+let sliderNum = 0;
+let sliderInterval = setInterval(nextImg, 3000);
+
+function nextImg() {
+    imgChange(sliderNum + 1)
+}
+
+function prevImg() {
+    imgChange(sliderNum - 1)
+}
+
+function imgChange(x) {
+    clearClass();
+    sliderNum = (x + imgLocations.length) % imgLocations.length;
+    heroImg.src = imgLocations[sliderNum];
+    addClass()
 
 }
 
+function pauseSlideShow() {
+    clearInterval(sliderInterval);
+}
+
 rightButton.addEventListener('click', function() {
-    imgChange(1);
+    nextImg(); pauseSlideShow();
 });
 leftButton.addEventListener('click', function() {
-    imgChange(-1)
+    prevImg(); pauseSlideShow();
 });
 
 
