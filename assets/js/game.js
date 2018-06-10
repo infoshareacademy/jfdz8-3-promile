d1a86ae95483cc4e10f11402131108fdfc694856
Michal Muszynski <m.muszy@gmail.com>
2018-06-10 13:01:30 +0200

    Game board functionality

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,12 @@
-var player;
+var player = "X";
 var score;
-var gameBoard = [
-    '          ',
-    '          ',
-    '          ',
-    '          ',
-    '          ',
-    '          ',
-    '          ',
-    '          ',
-    '          ',
-    '          '
-];
+var board = createBoard(10, 10);
 
+function generateBoard() {
+    var boardCell = document.createElement('div');
+    document.body.appendChild(boardCell);
+    boardCell.setAttribute("class", "cell")
+}
 
 function createBoard(x, y) {
     return Array(x).fill(0).map(function(element) {
@@ -20,4 +14,40 @@ function createBoard(x, y) {
     })
 }
 
-var board = createBoard(10, 10);
+var playerPosition = {
+    x: 0,
+    y: 0
+};
+
+// function setCurrentPosition() {
+//     board[playerPosition.x][playerPosition.y] = player;
+// }
+// setCurrentPosition();
+
+var moves = {
+  ArrowRight: function() {
+      playerPosition.x += 1
+  }
+};
+
+moves.ArrowRight(playerPosition);
+
+// function setCurrentPosition(x, y) {
+//    return board[x][y] = player
+// }
+//
+// setCurrentPosition(1,1);
+
+// window.addEventListener('keydown', function(event) {
+//     pressedKey = event.code
+// });
+
+
+
+function update() {
+    var board = createBoard(10, 10);
+    board[playerPosition.y][playerPosition.x] = player;
+    console.table(board)
+}
+
+
