ceec1b35c2d58ee457311ce74ac18fe1d7179c0e
Bartosz Frackiewicz <decomplosed@gmail.com>
2018-07-08 13:32:41 +0200

    added start screen and styles connected to it

---
+++ assets/js/gameLayout.js
@@ -0,0 +1,7 @@
+
+var playButton = document.getElementsByClassName('play');
+
+playButton.addEventListener('click', function () {
+    var startScreen = document.getElementsByClassName('startScreen');
+    startScreen.classList.add('in-view')
+})
