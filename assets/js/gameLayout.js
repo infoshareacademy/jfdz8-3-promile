a588cc46a073cfd46504f87f05290f03c04a0e65
bart <decomplosed@gmail.com>
2018-07-12 19:47:16 +0200

    updated startscreen

--- assets/js/gameLayout.js
+++ assets/js/gameLayout.js
@@ -1,7 +1,15 @@
 
-var playButton = document.getElementsByClassName('play');
+var playButton = document.querySelector(".play");
+var instructionsButton = document.querySelector(".instructions");
 
 playButton.addEventListener('click', function () {
-    var startScreen = document.getElementsByClassName('startScreen');
-    startScreen.classList.add('in-view')
-})
+    var gameContainer = document.querySelector(".gameContainer");
+    var startScreen = document.querySelector(".startScreen");
+    gameContainer.style.display = 'block';
+    startScreen.style.display = 'none';
+});
+
+instructionsButton.addEventListener('click', function () {
+    var instructions = document.querySelector(".instructions__container");
+    instructions.style.display = 'block';
+});
