84715140dd4072b50bcf5638d3b3c92b90d4b226
Michal Muszynski <m.muszy@gmail.com>
2018-07-13 20:33:27 +0200

    Changing attribute function

--- assets/js/formSubmit.js
+++ assets/js/formSubmit.js
@@ -1,14 +1,23 @@
 const submitButton = document.querySelector('.submit');
 const navigationLogo = document.querySelector('.center-logo');
+const centerLogo = document.querySelector('.anchor-link');
 const newLogoSource = 'assets/images/LOGO1_fire.png';
 const normalLogoSource = 'assets/images/LOGO1.png';
+const linkToGame = 'http://www.3-promile.jfdz8.is-academy.pl/game.html';
 
 submitButton.addEventListener('click', checkLocalStorage);
 navigationLogo.addEventListener('mouseover', function() {
     changeLogo(newLogoSource)
 });
+
 navigationLogo.addEventListener('mouseout', function() {
     changeLogo(normalLogoSource)
+});
+
+centerLogo.addEventListener('click', function () {
+    if (localStorage.getItem('submitted')) {
+        putNewAttributes()
+    }
 });
 
 function checkLocalStorage() {
@@ -21,4 +30,9 @@ function changeLogo(source) {
     if (localStorage.getItem(('submitted'))) {
         navigationLogo.src = source
     }
+}
+
+function putNewAttributes() {
+    centerLogo.setAttribute('href', linkToGame);
+    centerLogo.setAttribute('target', "_blank");
 }
