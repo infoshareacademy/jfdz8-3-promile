af57e122662a40133d21b613298cac2b9fc964b0
bart <decomplosed@gmail.com>
2018-07-27 21:01:46 +0200

    updated startscreen

--- assets/js/gameLayout.js
+++ assets/js/gameLayout.js
@@ -1,15 +1,38 @@
 
 var playButton = document.querySelector(".play");
 var instructionsButton = document.querySelector(".instructions");
+var instructionsBackButton = document.querySelector('.back');
+var buttonsContainer = document.querySelector('.buttons__container');
+var instructions = document.querySelector(".instructions__container");
+var gameContainer = document.querySelector(".gameContainer");
+var startScreen = document.querySelector(".startScreen");
+var chooseLevel = document.querySelector(".chooseLevel");
+var levelButton = document.querySelectorAll(".level");
+
 
 playButton.addEventListener('click', function () {
-    var gameContainer = document.querySelector(".gameContainer");
-    var startScreen = document.querySelector(".startScreen");
-    gameContainer.style.display = 'block';
     startScreen.style.display = 'none';
+    gameContainer.style.display = 'block';
 });
 
 instructionsButton.addEventListener('click', function () {
     var instructions = document.querySelector(".instructions__container");
+    buttonsContainer.style.display = 'none';
     instructions.style.display = 'block';
 });
+
+instructionsButton.addEventListener('click', function () {
+    instructions.style.display = 'block';
+    buttonsContainer.style.display = 'none';
+});
+
+instructionsBackButton.addEventListener('click', function () {
+    buttonsContainer.style.display = 'flex';
+    instructions.style.display = 'none';
+});
+
+levelButton.addEventListener('click', function () {
+    gameContainer.style.display = 'block';
+    chooseLevel.style.display = 'none';
+});
+
