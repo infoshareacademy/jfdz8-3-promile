5e4e84ed1adf559ba07805fa2ab7bfd46dfb3d94
bart <decomplosed@gmail.com>
2018-07-28 02:09:07 +0200

    close generating random images in functions

--- assets/js/gameLayout.js
+++ assets/js/gameLayout.js
@@ -9,7 +9,7 @@ var startScreen = document.querySelector(".startScreen");
 var chooseLevel = document.querySelector(".chooseLevel");
 var levelButton = document.querySelector(".level");
 var cityBackground = document.querySelector(".wall");
-
+var skillToCollect = document.querySelector(".ghost");
 
 playButton.addEventListener('click', function () {
     startScreen.style.display = 'none';
@@ -37,17 +37,29 @@ instructionsBackButton.addEventListener('click', function () {
 //     chooseLevel.style.display = 'none';
 // });
 
+function generateCity() {
+    var randomCity = Math.floor(Math.random() * 10) + 1;
+    var cityImages = [
+        "url('assets/images/city_background/1.png')",
+        "url('assets/images/city_background/2.png')",
+        "url('assets/images/city_background/3.png')",
+        "url('assets/images/city_background/4.png')",
+        "url('assets/images/city_background/5.png')",
+        "url('assets/images/city_background/6.png')",
+        "url('assets/images/city_background/7.png')",
+        "url('assets/images/city_background/8.png')",
+        "url('assets/images/city_background/9.png')",
+        "url('assets/images/city_background/10.png')"];
+    cityBackground.style.backgroundImage = cityImages[randomCity];
+}
 
-    var random = Math.floor(Math.random() * 10) + 1;
-    var images = [
-        "url('../images/city_background/1.png')",
-        "url('../images/city_background/2.png')",
-        "url('../images/city_background/3.png')",
-        "url('../images/city_background/4.png')",
-        "url('../images/city_background/5.png')",
-        "url('../images/city_background/6.png')",
-        "url('../images/city_background/7.png')",
-        "url('../images/city_background/8.png')",
-        "url('../images/city_background/9.png')",
-        "url('../images/city_background/10.png')"];
-    cityBackground.style.backgroundImage = images[random];
+function generateSkill() {
+    var randomSkill = Math.floor(Math.random() * 5) + 1;
+    var skillImages = [
+        "url('assets/images/skills/react.png')",
+        "url('assets/images/skills/css.png')",
+        "url('assets/images/skills/html.png')",
+        "url('assets/images/skills/javascript.png')",
+        "url('assets/images/skills/nodejs.png')"];
+    skillToCollect.style.backgroundImage = skillImages[randomSkill];
+}
