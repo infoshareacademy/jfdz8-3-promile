3f09cf526cee110598f7e57f630f4631409b45e3
Bartosz Zawada <bartosh.zavada@gmail.com>
2018-08-04 13:15:39 +0200

    add shake effect to cookie monster

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -37,6 +37,7 @@
                 monster_container = document.createElement('div');
             monster_container.setAttribute('id','monster-container');
             cookiemonster.setAttribute('id','cookiemonster');
+            cookiemonster.classList.add('shake')
             cookiebubble.setAttribute('id','cookiebubble');
             cookiebubble.innerHTML = 'Do you want some cookies?';
             container.appendChild(monster_container);
@@ -56,6 +57,7 @@
                     element2.style.bottom = position + 'px'))
                 }
             }
+
         }
         return monsterMove()
     }
